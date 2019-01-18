c1f9f87c16ebc81cdf84c9a063b9efe1dde9dc53
yogesh519 <yogesh.rawat@appanalytics.in>
2019-01-18 17:39:45 +0530

    imp.php

---
+++ imp.php
@@ -0,0 +1,9 @@
+<html>
+<head>
+</head>
+<body>
+<form  method='POST' enctype='multipart/form-data' action ="quick.php">
+Upload CSV FILE: <input type='file' name='csv_info' id ='id1' /> 
+<input type='submit' name='submit' value='Upload File' />
+</form>
+</body>
